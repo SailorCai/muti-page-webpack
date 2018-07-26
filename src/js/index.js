c7687f9ce40b88c2602f3394c2d392013812d4e3
logan <1187023819@qq.com>
2018-07-26 10:36:01 +0800

    add some explanation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '@/css/index.css';
+import _ from "lodash";
 
 function index(){
 	console.log('hello webpack!');
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -10,15 +10,20 @@ let entrys = {};
 let htmlPlugins = [];
 
 for(let item of htmlArr){
+	//我们只需要.html前面的文件名
 	let name = item.split('.html')[0];
 	htmlPlugins.push(new WebpackHtmlPlugin({
 		filename: item,
 		template: path.resolve(__dirname, `src/html/${item}`),
+		//common代表公共模块，name就是html对应的同名js文件
+		//这个配置将会自动在生成的html中插入你指定的js
 		chunks: ['common', name]
 	}));
+	//配置入口
 	entrys[name] = `./src/js/${name}.js`;
 };
 module.exports = merge(base, {
+	//传入entry配置
 	entry: entrys,
 	devtool: "cheap-module-eval-source-map",
 	devServer: {
@@ -30,6 +35,7 @@ module.exports = merge(base, {
 		open: true
 	},
 	plugins: [
+		//传入html-webpack-plugin配置对象
 		...htmlPlugins,
 	]
 });
